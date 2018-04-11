6061ec19c3d9a69607a9a99831776c1e9ccb4bd5
Ankit Pati <contact@ankitpati.in>
2018-04-11 14:38:50 +0530

    SOAP Service: factorial of a number

---
+++ perl/WebSOA/SOAP/Services/Factorial.pm
@@ -0,0 +1,15 @@
+package Factorial;
+
+use SOAP::Lite;
+
+sub getFactorial {
+    my $self = shift;
+    my $n = shift;
+
+    my $factorial = 1;
+    $factorial *= $_ foreach 1 .. $n;
+
+    return $factorial;
+}
+
+1;
