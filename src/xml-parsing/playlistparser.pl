da35739794a82d90ccf32f643121dd4f926e5c57
Ankit Pati <contact@ankitpati.in>
2018-05-14 14:06:33 +0530

    parsing XML using XML::LibXML

---
+++ src/xml-parsing/playlistparser.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+
+package AnkitPati::PlaylistParser;
+
+use strict;
+use warnings;
+
+use XML::LibXML;
+
+sub main {
+    @ARGV == 1 or die "Usage:\n\t${\(split m|/|, $0)[-1]} <filename>\n";
+    my $filename = shift @ARGV;
+
+    my $dom = eval { XML::LibXML->load_xml (location => $filename) };
+    die "Malformed XML file!\n" if $@;
+
+    foreach my $movie ($dom->findnodes ('//movie')) {
+        my $cast = join ', ', map { $_->to_literal }
+                                $movie->findnodes ('./cast/person/@name');
+        $cast =~ s/, $//;
+
+        print "\nTitle    : ", $movie->findvalue ('./title');
+        print "\nDirector : ", $movie->findvalue ('./director');
+        print "\nRelease  : ", $movie->findvalue ('./release-date');
+        print "\nMPAA     : ", $movie->findvalue ('./mpaa-rating');
+        print "\nTime     : ", $movie->findvalue ('./running-time');
+        print "\nCast     : ", $cast;
+        print "\n"
+    }
+}
+
+main unless caller;
+
+1;
