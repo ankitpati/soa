07c506ca9098cb10916905e4670cb238bd19f8bf
Ankit Pati <contact@ankitpati.in>
2018-05-15 18:53:07 +0530

    remove XML parsing code in PHP

--- src/XML/xmlparse.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env php
-<?php
-
-sizeof($argv) === 2 or die("Usage:\n\txmlparse.php <filename>\n");
-
-($users = @simplexml_load_file($argv[1])) !== false
-    or die("$argv[1] cannot be opened.\n");
-
-print_r($users);
